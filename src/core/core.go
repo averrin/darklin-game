1a44453eda1d61edec3022bde678bc78580a147d
Alexey Nabrodov <alexey.nabrodov@dev.zodiac.tv>
2016-01-15 18:14:38 +0300

    disable readline

--- src/core/core.go
+++ src/core/core.go
@@ -3,9 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
-
-	"gopkg.in/readline.v1"
 )
 
 // TestActor just someone who do something
@@ -55,37 +52,37 @@ func main() {
 	gs.Streams["time"] = ts.Stream
 
 	http.HandleFunc("/ws", gs.CmdHandler)
-	log.Println("Serving")
+	log.Println("Serving at :80")
 	// http.Handle("/", http.FileServer(http.Dir(".")))
 	go http.ListenAndServe(":80", nil)
-	go gs.Live()
+	gs.Live()
 
-	var completer = readline.NewPrefixCompleter(
-		readline.PcItem("time"),
-		readline.PcItem("exit"),
-	)
-	rl, err := readline.NewEx(&readline.Config{
-		Prompt:       ">> ",
-		HistoryFile:  "/tmp/readline.tmp",
-		AutoComplete: completer,
-	})
-	if err != nil {
-		panic(err)
-	}
-	defer rl.Close()
-	log.SetOutput(rl.Stderr())
-	log.SetPrefix("")
-
-	for {
-		line, err := rl.Readline()
-		if err != nil { // io.EOF
-			log.Fatal(err)
-			break
-		}
-		if line == "" {
-			continue
-		}
-		println("<< ", line)
-		stream <- Event{time.Now(), COMMAND, line, "cmd"}
-	}
+	// var completer = readline.NewPrefixCompleter(
+	// 	readline.PcItem("time"),
+	// 	readline.PcItem("exit"),
+	// )
+	// rl, err := readline.NewEx(&readline.Config{
+	// 	Prompt:       ">> ",
+	// 	HistoryFile:  "/tmp/readline.tmp",
+	// 	AutoComplete: completer,
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer rl.Close()
+	// log.SetOutput(rl.Stderr())
+	// log.SetPrefix("")
+	//
+	// for {
+	// 	line, err := rl.Readline()
+	// 	if err != nil { // io.EOF
+	// 		log.Fatal(err)
+	// 		break
+	// 	}
+	// 	if line == "" {
+	// 		continue
+	// 	}
+	// 	println("<< ", line)
+	// 	stream <- Event{time.Now(), COMMAND, line, "cmd"}
+	// }
 }
